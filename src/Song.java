c8458e82949df68e8e8e6b159afb4e692b1a5c46
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-19 03:30:38 +0600

    Music Playlist Manager

---
+++ src/Song.java
@@ -0,0 +1,18 @@
+class Song {
+    private final String title;
+    private final String artist;
+    private final String genre;
+    private final String mood;
+
+    public Song(String title, String artist, String genre, String mood) {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.mood = mood;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Artist: " + artist + ", Genre: " + genre + ", Mood: " + mood;
+    }
+}
